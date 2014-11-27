a88b4c2941dfe435f241c648041230818e337ea8
MK <kohenkatz@gmail.com>
2014-11-27 00:41:51 -0500

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "moshe@ymkatz.net"
 license          "MIT"
 description      "Installs Beanstalk Console"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.0"
+version          "0.3.1"
 supports         "ubuntu"
 
 depends "php" #Actually depends on MY php cookbook!
